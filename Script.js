0a9ddcc0f5809b7cbf576e3875911304a00a0e3a
Italo Nobre <italonobre@ads.fiponline.edu.br>
2022-04-26 13:56:44 -0300

    Atualização do Projeto, foram implantas novos detalher de Estilo e niveis de Classificação de Peso

--- Script.js
+++ Script.js
@@ -6,11 +6,29 @@ function meuEscopo(){
 const pessoas = [];
 
 function recebeEventoForm (evento){
+    
     evento.preventDefault();
     const nome = document.querySelector('.nome');
     const sobrenome = document.querySelector('.sobrenome');
     const peso = document.querySelector('.peso');
     const altura = document.querySelector('.altura');
+    const altura1 = Number(altura.value);
+    const peso1 = Number(peso.value)
+    const imcTotal = peso1 / (altura1 * 2);
+       
+    if (imcTotal > 40){
+        resulImc = "OBESIDADE GRAVE";
+
+    }else if(imcTotal > 30){
+         resulImc = "OBESIDADE";
+
+    }else if(imcTotal > 25){
+        resulImc = "SOBREPESO";
+    }else if(imcTotal > 18){
+            resulImc = "NORMAL";
+    }else{
+        resulImc = "ABAIXO DO PESO";
+    }
 
     pessoas.push({
 
@@ -21,26 +39,13 @@ function recebeEventoForm (evento){
 
 
     });
-    console.log(pessoas);
-
-    resultado.innerHTML += `<p> ${nome.value} ${sobrenome.value}` +
-    `${peso.value} ${altura.value}</p>`;
-
-
+    
+    resultado.innerHTML += `<div id="teste" ><p> ${nome.value} ${sobrenome.value}</br>` +
+    ` Peso: ${peso.value}</br> Altura:  ${altura.value}</br>
+    IMC: ${imcTotal.toFixed(2)}</p></br>
+    Classificação de Peso: ${resulImc}</p></div>`;
 
 }
     form.addEventListener('submit',recebeEventoForm);
 }
 meuEscopo();
-
-   
- 
-
-    
-    
-   
-
-
-
-
-
